7a1243eca6af2b480a675e9ea4e3858232be2a74
xiaobeiHan <hanjie289@163.com>
2016-12-10 17:45:31 +0800

    增加代码

---
+++ src/Hj.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Hj extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req,HttpServletResponse resp)
+	throws IOException,ServletException{
+		resp.getWriter().write("FirstServlet"+new Date());
+	}
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+}			
+
+
+
